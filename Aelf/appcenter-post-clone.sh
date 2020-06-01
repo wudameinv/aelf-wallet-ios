64b1556e62330bcc295666fba8c99f11c94b0000
wudameinv <huihui.wu@aelf.io>
2020-06-01 15:55:11 +0800

    test

--- Aelf/appcenter-post-clone.sh
+++ Aelf/appcenter-post-clone.sh
@@ -4,6 +4,6 @@ echo "Uninstalling all CocoaPods versions"
 sudo gem uninstall cocoapods --all --executables
 COCOAPODS_VER=`sed -n -e 's/^COCOAPODS: \([0-9.]*\)/\1/p' Podfile.lock`
 echo "Installing CocoaPods version $COCOAPODS_VER"
-sudo gem install cocoapods 1.5.3
+sudo gem install cocoapods-1.5.3
 
 
