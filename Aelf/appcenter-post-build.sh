3b9b1282c8f485628e6870657ab8cb353b190e4f
wudameinv <53204268+wudameinv@users.noreply.github.com>
2020-06-19 17:50:53 +0800

    Update appcenter-post-build.sh

--- Aelf/appcenter-post-build.sh
+++ Aelf/appcenter-post-build.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
+ls -l  /Users/runner/runners/2.170.1/work/1/output/build/export/_XcodeTaskExport_AelfApp/
 sudo gem install fir-cli
 fir p $APPCENTER_OUTPUT_DIRECTORY/$APP_FILE -T $FIR_TOKEN
